edeca5509fa8383babe56e72765060077c0dce89
jhneu <jhneu@192.168.1.108>
2022-10-02 16:41:32 +0900

    10.02 1. 기존 resources 폴더 붙여넣기 (layout관련) 2. 기존 controller 폴더 붙여넣기 3. build.gradle 수정 	- mysql 드라이버 의존성 주입 	- thymeleaf layout 의존성 주입 	- security 관련 의존성 추가 (주석) 4. .properties 파일 수정

---
+++ src/main/java/com/project/controller/MemberController.java
@@ -0,0 +1,21 @@
+package com.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+	
+	@GetMapping("/login")
+	public String login() {
+		return "contents/login";
+	}
+	
+	@GetMapping("/register")
+	public String register() {
+		return "contents/register";
+	}
+
+}
